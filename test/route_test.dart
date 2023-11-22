85bb38578732ca9bbb4791bbae6bb7b16d457069
Bui Nguyen Nhat Tan <buinguyennhattan12122002@gmail.com>
2023-11-22 22:49:12 +0700

    router test

--- test/route_test.dart
+++ test/route_test.dart
@@ -1,37 +1,79 @@
+// ignore_for_file: unused_local_variable, prefer_final_locals
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:template/core/helper/izi_timezone.dart';
+import 'package:template/core/utils/color_resources.dart';
+import 'package:template/data/repositories/auth_repositories.dart';
+import 'package:mockito/mockito.dart';
+import 'package:template/core/di_container.dart' as di;
 import 'package:template/main.dart';
-import 'package:template/presentation/pages/begin_screen/onboarding/UI_model.dart';
-import 'package:template/presentation/pages/begin_screen/onboarding/onboarding.dart';
 import 'package:template/presentation/pages/begin_screen/onboarding/onboarding_controller.dart';
 
-void main() {
+class MockAuthRepository extends Mock implements AuthRepository {}
+
+void main() async {
+  // ignore: await_only_futures
+  // await WidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  await di.init(); // dùng để khởi tạo các service, repository, controller
+  SharedPreferences.setMockInitialValues({});
+  // Initialize mobile ads.
+  MobileAds.instance.initialize();
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.light,
+    statusBarIconBrightness: Brightness.light,
+  ));
+
+  IZITimeZone().initializeTimeZones();
+
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 1500)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..indicatorType = EasyLoadingIndicatorType.threeBounce
+    ..progressColor = ColorResources.WHITE
+    ..backgroundColor = ColorResources.PRIMARY_1
+    ..indicatorColor = ColorResources.WHITE
+    ..textColor = ColorResources.WHITE
+    ..maskColor = Colors.transparent
+    ..userInteractions = false
+    ..textStyle = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: ColorResources.WHITE,
+    )
+    ..dismissOnTap = false;
+
+  PackageDescriptionController packageDescriptionController;
+  PageController _pageController;
+  AuthRepository authRepository = GetIt.instance<AuthRepository>();
+
+  setUp(() {
+    packageDescriptionController = PackageDescriptionController();
+    _pageController = PageController();
+    authRepository = GetIt.instance<AuthRepository>();
+  });
+
   testWidgets('Splash screen navigates to introduce screen',
       (WidgetTester tester) async {
-    late PackageDescriptionController packageDescriptionController;
-    late PageController _pageController;
+    final authRepository = MockAuthRepository();
 
-    setUp(() => {
-          packageDescriptionController = PackageDescriptionController(),
-          _pageController = PageController(),
-        });
-    testWidgets("checking router splash", (widgetTester) async {
-      await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp());
 
-      await tester.pump(Duration(seconds: 2));
+    await tester.pump(const Duration(seconds: 2));
 
-      // Kiểm tra xem đã chuyển đến trang introduce hay chưa
-      expect(
-          find.byWidget(PageView.builder(
-            controller: _pageController,
-            itemCount: payViews.length,
-            itemBuilder: (context, index) {
-              return Onboarding(
-                payViews[index],
-              );
-            },
-          )),
-          findsOneWidget);
-    });
+    // Check for a widget that is present in the introduce screen
+    expect(find.text('TextInIntroduceScreen'), findsOneWidget);
+    expect(1, 1);
   });
 }
