a143ce1538860edb26620a9830386f010b27b8d2
Bui Nguyen Nhat Tan <buinguyennhattan12122002@gmail.com>
2023-12-17 16:16:31 +0700

    setting for sonar and output

--- sonar.bat
+++ sonar.bat
@@ -4,4 +4,4 @@ call flutter pub get
 call flutter test
 call flutter test --machine --coverage > tests.output
 
-call docker-repositories\Sonar-Docker\sonar-scanner-5.0.1.3006-windows\bin\sonar-scanner
+call D:\sonarqube-server\sonar-scanner-5.0.1.3006-windows\bin\sonar-scanner.bat
