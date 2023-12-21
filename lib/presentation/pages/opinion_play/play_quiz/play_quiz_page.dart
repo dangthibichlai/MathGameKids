30d64b6e1ee1a6fc5152b4edc75cf5424d0f7272
Bui Nguyen Nhat Tan <buinguyennhattan12122002@gmail.com>
2023-12-21 17:12:45 +0700

    onclose

--- lib/presentation/pages/opinion_play/play_quiz/play_quiz_page.dart
+++ lib/presentation/pages/opinion_play/play_quiz/play_quiz_page.dart
@@ -3,12 +3,10 @@ import 'package:get/get.dart';
 import 'package:template/core/base_widget/izi_app_bar.dart';
 import 'package:template/core/helper/izi_size_util.dart';
 import 'package:template/core/utils/color_resources.dart';
-import 'package:template/presentation/pages/dash_board/dash_board_controller.dart';
 import 'package:template/presentation/pages/opinion_play/extend_back_ads.dart';
 import 'package:template/presentation/pages/opinion_play/play_quiz/play_quiz_controller.dart';
 import 'package:template/presentation/pages/opinion_play/widget/showAnswer.dart';
 import 'package:template/presentation/pages/opinion_play/widget/show_question.dart';
-import 'package:template/presentation/widgets/banner_ads.dart';
 
 class PlayQuiz extends GetView<playQuizController> {
   @override
